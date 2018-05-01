8e3ef1487bc5892e689e4476722e77169885c52c
chloesteph <32639349+chloesteph@users.noreply.github.com>
2018-05-01 12:50:02 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,41 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-// https://image.ibb.co/euc3hb/edge.png
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-         <div className='side-bar'> chloe u r a idiot</div>
-         <div className='main-screen'></div>
-     </div>
+      <div className="body">
+
+        <div className="left">
+            <div className="pink">
+                <div className="chloe">Chloe</div>
+                <div className="steph">Steph</div>
+                <div className="design">designs</div>
+            </div>
+        </div>
+
+        <div className="right">
+            <div className="work">Work</div> 
+            <div className="who">Who Is She</div>
+            <div className="pro1">project name</div>
+            <div className="pro2">project name</div>
+            <div className="pro3">project name</div>
+            <div className="pro4">project name</div>
+
+            <span className="bottom">
+              <div className="about">about me</div>
+              <div className="contact">contact</div>
+            </span>
+
+
+        </div>
+
+      </div>
     );
   }
 }
 
 export default App;
+  
